a2e918838cf23e06806f27ad34133190efe993af
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-05-31 14:07:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# Made with python3
+# (C) @FayasNoushad
+# Copyright permission under MIT License
+# All rights reserved by FayasNoushad
+# License -> https://github.com/FayasNoushad/Channel-Message-Editor/blob/main/LICENSE
+
 import os
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
